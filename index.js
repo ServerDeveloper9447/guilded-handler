9dc8b30b207cbd3bdf9682b16c24ef90e9d8c46a
ServerDeveloper9447 <soumalyaghosh2022@gmail.com>
2022-12-22 14:39:00 +0530

    Updated to limit event listeners

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// javascript is really confusing ngl
 const fs = require('fs')
 const chalk = require('chalk')
 class guildedHandler extends Error {
@@ -13,10 +14,11 @@ const walk = require('walk')
  * @param {String} path Path to the commands folder
  * @param {Object} client The guilded API client object
  */
-module.exports = (client,config = {path_to_dir:path,prefix:"!"}) => {
+module.exports = (client,config = {path_to_dir:path,prefix:""}) => {
   if (!client) throw new guildedHandler("Must provide app where client = Guilded.js client object")
   if (!config.path_to_dir) throw new guildedHandler("Must provide path where path_to_dir = the path to the directory containing all command files (js).")
   const pathogen = require('path').resolve(config.path_to_dir)
+  client.options.path = pathogen
   try {
     fs.accessSync(pathogen, fs.R_OK)
   } catch (err) {
@@ -34,6 +36,9 @@ module.exports = (client,config = {path_to_dir:path,prefix:"!"}) => {
   
   walker.on('end',() => {
     var commandcount = 0
+    var commands = []
+    const prefarr = [].concat(config.prefix)
+    client.options.prefix = config.prefix
     files.forEach(path => {
     console.log(chalk.greenBright(`Walking into ${path}`))
     try {
@@ -41,9 +46,16 @@ module.exports = (client,config = {path_to_dir:path,prefix:"!"}) => {
     if(!command.name || !command.run) return console.log(chalk.redBright(`Failed to walk in: ${path}`)),console.log(chalk.redBright("name or run parameter empty"));
       console.log(chalk.green(`Loaded command ${command.name}\n----------------------------`))
       commandcount++
-      const arr = [].concat(command.name,command.aliases).filter(x => x!=null)
-       const prefarr = [].concat(config.prefix)
-      client.on('messageCreated',message => {
+      const aliases = [].concat(command.aliases)
+      const cnames = [].concat(command.name,aliases.filter(x => x!=null))
+      commands.push({name:cnames,run:command.run})
+    } catch(err) {
+      console.warn(chalk.redBright(`Failed to walk in: ${path}.`))
+      throw new guildedHandler(err)
+    }
+  })
+    client.on('messageCreated',message => { //only to limit this mf i had to change the whole structure
+      if(message.createdById == client.user.id) return;
         if(!prefarr.some(w => message.content.toLowerCase().startsWith(w.toLowerCase()))) return;
         var prefixforthis = "";
         prefarr.forEach(el => {
@@ -52,19 +64,28 @@ module.exports = (client,config = {path_to_dir:path,prefix:"!"}) => {
             prefixforthis = el.toLowerCase()
           }
         })
-        const messagearr = message.content.slice(prefixforthis.length).split(" ")
-        const commandname = messagearr[0]
-        messagearr.shift()
-        if(!arr.some(w => commandname.toLowerCase() == w.toLowerCase())) return;
-        command.run(client,message,messagearr)
+      const messagearr = message.content.slice(prefixforthis.length).split(" ")
+      const commandname = messagearr[0]
+      messagearr.shift()
+      const commandnames = []
+      commands.map(x => x.name).forEach(x => {
+        x.forEach(m => commandnames.push(m.toLowerCase()))
       })
-    } catch(err) {
-      console.warn(chalk.redBright(`Failed to walk in: ${path}.`))
-      throw new guildedHandler(err)
-    }
-  })
+      var cname = ''
+      commandnames.some(w => {
+        if(!cname) {
+         if(commandname.toLowerCase() == w) {
+            cname = w;
+           return true;
+          } else return false;
+        }
+      })
+      const command = commands.filter(x => x.name.includes(cname.toLowerCase()))[0]
+      if(!command) return;
+      command.run(client,message,messagearr)
+    })
     client.on('ready',() => {
-      console.log(chalk.blue(`Bot            : ${client.user.name}\nID             : ${client.user.id}\nBot ID         : ${client.user.botId}\nPrefix         : ${config.prefix.join(", ").toLowerCase()}\nTotal Commands : ${commandcount}`))
+      console.log(chalk.blue(`Bot            : ${client.user.name}\nID             : ${client.user.id}\nBot ID         : ${client.user.botId}\nPrefix         : ${prefarr.join(", ").toLowerCase()}\nTotal Commands : ${commandcount}`))
       console.log(`${chalk.blue("Status         :")} ${chalk.green("Ready")}`)
       console.log(`Guilded-Handler Version: ${require(__dirname+'/package.json').version} || ${client.user.name} || Power Guilds Development Team || https://guilded.gg/powerguilds`)
     })
